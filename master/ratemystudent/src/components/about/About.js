f8697a66db0941c445be7a7a343cf4092f78074d
Basu <0001bz@gmail.com>
2019-08-03 18:38:10 -0400

    Add files via upload

--- master/ratemystudent/src/components/about/About.js
+++ master/ratemystudent/src/components/about/About.js
@@ -7,7 +7,10 @@ export default class About extends Component {
           <div className="card z-depth-0">
            <div className="card-content">
             <span className="card-title">About Rate My Student</span>
-              <p></p>
+              <p>At Rate My Students, Professors can create profiles of their beloved students or their worst experience.
+This is a chance for students who want to increase their good reputation, to display to hiring managers, recruiters, and organizations.
+The idea of Rate My Students  is to create a public community where professors can provide ratings and recomendations for their students.
+Rate My Students Allow the users to create profiles of the students and comment on them, describing their behaviors</p>
            </div>
           </div>
         </div>
